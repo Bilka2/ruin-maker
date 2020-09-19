22c8dcc607a181f164210bc65bae05e67790464c
Bilka <Bilka2@users.noreply.github.com>
2020-09-19 17:06:05 +0200

    Implemented output for new ruin data structure

--- output.lua
+++ output.lua
@@ -5,60 +5,90 @@ local output
 local function set_tiles(tiles, tile_filter, center, out)
   if next(tiles) == nil then return end
 
-  out[#out+1] = "    surface.set_tiles({\n"
+  out[#out+1] = "  tiles =\n"
+  out[#out+1] = "  {\n"
   local before_size = table_size(out)
 
   for _, tile in pairs(tiles) do
     if tile_filter[tile.name] then
       local vec = util.vector_from_center(tile.position, center)
 
-      out[#out+1] = "            {name = \"" .. tile.name .. "\", position = "
-      out[#out+1] = "{center.x + (" .. vec.x .. "), center.y + (" .. vec.y .. ")}},\n"
+      out[#out+1] = "    {\"" .. tile.name .. "\", "
+      out[#out+1] = "{x = " .. vec.x .. ", y = " .. vec.y .. "}},\n"
     end
   end
 
   if before_size == table_size(out) then -- no tiles added
-    out[#out] = nil -- remove "    surface.set_tiles({\n"
+    out[#out] = nil -- remove "  {\n"
+    out[#out] = nil -- remove "  tiles =\n"
     return
   end
 
-  out[#out+1] = "                               }, true)\n"
+  out[#out+1] = "  }\n"
 end
 
 local function create_entity(entities, center, out)
   if next(entities) == nil then return end
 
-  out[#out+1] = "    local ce = function(params)\n"
-  out[#out+1] = "        params.raise_built = true\n"
-  out[#out+1] = "        return surface.create_entity(params)\n"
-  out[#out+1] = "    end\n"
-  out[#out+1] = "    local fN = game.forces.neutral\n"
-  out[#out+1] = "    local direct = defines.direction\n"
+  out[#out+1] = "  entities =\n"
+  out[#out+1] = "  {\n"
 
   for _, entity in pairs(entities) do
     local vec = util.vector_from_center(entity.position, center)
 
-    out[#out+1] = "    ce{name = \"" .. entity.name .. "\", position = "
-    out[#out+1] = "{center.x + (" .. vec.x .. "), center.y + (" .. vec.y .. ")}"
-    if entity.direction ~= defines.direction.north then
-      out[#out+1] = ", direction = direct." .. util.direction_to_str(entity.direction)
+    out[#out+1] = "    {\"" .. entity.name .. "\", "
+    out[#out+1] = "{x = " .. vec.x .. ", y = " .. vec.y .. "}"
+
+    out[#out+1] = ", {" -- extra options start
+
+    local dir = entity.direction
+    if dir ~= defines.direction.north then
+      out[#out+1] = "dir = \"" .. util.direction_to_str(dir) .. "\", "
     end
-    out[#out+1] = ", force=fN}\n"
+    if entity.is_entity_with_owner then
+      out[#out+1] = "force = \"neutral\", "
+    end
+    if entity.is_entity_with_health and (entity.get_health_ratio() ~= 1) then
+      local dmg = math.floor(entity.prototype.max_health - entity.health)
+      out[#out+1] = "dmg = {dmg = " .. dmg .. "}, "
+    end
+    if entity.has_items_inside() then
+      local inv = nil
+      if entity.type == "container" then
+        inv = entity.get_inventory(defines.inventory.chest)
+      end
+      if entity.type == "ammo-turret" then
+        inv = entity.get_inventory(defines.inventory.turret_ammo)
+      end
+      if inv then
+        out[#out+1] = "items = " .. serpent.line(inv.get_contents()) .. ", "
+      end
+    end
+    if entity.type == "assembling-machine" then
+      local recipe = entity.get_recipe()
+      if recipe then
+        out[#out+1] = "recipe = \"" .. recipe.name .. "\", "
+      end
+    end
+    out[#out+1] = "}},\n" -- extra options and entity end
   end
+
+  out[#out+1] = "  },\n"
 end
 
 output = function(entities, tiles, tile_filter, center, player_index)
   local out = {}
-  out[1] = "return function(center, surface)\n"
+  out[1] = "return\n"
+  out[2] = "{\n"
   create_entity(entities, center, out)
   set_tiles(tiles, tile_filter, center, out)
 
-  if out[2] == nil then
+  if out[3] == nil then
     game.get_player(player_index).print("Nothing selected")
     return
   end
 
-  out[#out+1] = "end\n"
+  out[#out+1] = "}\n"
   log(table.concat(out))
 end
 
